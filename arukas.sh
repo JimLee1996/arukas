acbfcad1379d19e7ee0ba2817414d0fb650c7cbf
JimLee1996 <59061463@qq.com>
2016-12-31 10:25:16 +0800

    Add files via upload

---
+++ arukas.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#######################
+#					  #
+#	Arukas Api Tool	  #
+#					  #
+#	   Jim Lee		  #
+#    				  #
+#######################
+
+# Authorization
+token=9637690d-f5e2-47e3-ab41-a63cb2eb27c9
+secret=o6Io7gs7rZZX8nXzxphvtwhwZcFH9Jt9kd4b16ePlbkeJqNXJME15mnKpqQ8P1tJ
+
+data=`curl --user ${token}:${secret} https://app.arukas.io/api/containers -H "Content-Type: application/vnd.api+json" -H "Accept: application/vnd.api+json"`
+
+# Resolve Json
+host=`echo $data | sed 's/.*"host":\([^,}]*\).*/\1/'`
+port=`echo $data | sed 's/.*"service_port":\([^,}]*\).*/\1/'`
+
+echo $host
+echo $port
