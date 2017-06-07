370c3babd393e0738b3851874e3c9e22d38654b6
JimLee1996 <59061463@qq.com>
2017-06-07 20:02:46 +0800

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,11 +1,11 @@
 
-token = 'ba1cc5b3-dace-48fe-a3cd-e9e502c42346'
-secret = 'NYwYyQuB6a2B4bsF9Cds1sdMBxhSE1Pjy4oi1w7zPKonwPCgJgM6jSp2hWtXi5FO'
+token = ' '
+secret = ' '
 
 db_config = {
           'host':'127.0.0.1',
           'port':3306,
-          'user':'root',
-          'password':'Liji19960925',
+          'user':'',
+          'password':'',
           'db':'arukas'
           }
